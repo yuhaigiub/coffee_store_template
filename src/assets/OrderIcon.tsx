1d7d5c7e3f412d4a711267fa903e2a17dfec1004
Yuhaigiub <buigiahuynt@gmail.com>
2023-05-18 15:50:09 +0700

    change stroke-width

--- src/assets/OrderIcon.tsx
+++ src/assets/OrderIcon.tsx
@@ -12,14 +12,14 @@ const OrderIcon = () => {
 			<path
 				d="M9.62 16L11.12 17.5L14.37 14.5"
 				stroke="#000"
-				strokeWidth="0.5"
+				strokeWidth="0.8"
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
 			<path
 				d="M8.81 2L5.19 5.63"
 				stroke="#000"
-				strokeWidth="0.5"
+				strokeWidth="0.8"
 				strokeMiterlimit={10}
 				strokeLinecap="round"
 				strokeLinejoin="round"
@@ -27,7 +27,7 @@ const OrderIcon = () => {
 			<path
 				d="M15.19 2L18.81 5.63"
 				stroke="#000"
-				strokeWidth="0.5"
+				strokeWidth="0.8"
 				strokeMiterlimit={10}
 				strokeLinecap="round"
 				strokeLinejoin="round"
@@ -35,12 +35,12 @@ const OrderIcon = () => {
 			<path
 				d="M2 7.84998C2 5.99998 2.99 5.84998 4.22 5.84998H19.78C21.01 5.84998 22 5.99998 22 7.84998C22 9.99998 21.01 9.84998 19.78 9.84998H4.22C2.99 9.84998 2 9.99998 2 7.84998Z"
 				stroke="#000"
-				strokeWidth="0.5"
+				strokeWidth="0.8"
 			/>
 			<path
 				d="M3.5 10L4.91 18.64C5.23 20.58 6 22 8.86 22H14.89C18 22 18.46 20.64 18.82 18.76L20.5 10"
 				stroke="#000"
-				strokeWidth="0.5"
+				strokeWidth="0.8"
 				strokeLinecap="round"
 			/>
 		</svg>
